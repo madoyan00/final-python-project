0b4245e16e3b5c5780c45541767042faf2f54713
madoyan00 <150048135+madoyan00@users.noreply.github.com>
2025-04-05 17:11:53 +0400

    Delete rentCar directory

--- rentCar/rentCar/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RentcarConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rentCar'
--- rentCar/rentCar/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Car
-
-
-class CarForm(forms.ModelForm):
-    class Meta:
-        model= Car
-        fields = ['brand', 'model', 'year', 'price', 'transmission', 'phoneNumber', 'location', 'image']
